aac327c369c2a11d622fd70178a39ffeb103ca4b
aqsa2106 <aqsatech12@gmail.com>
2025-01-28 13:19:07 +0700

    V1.1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
+import './index.css'; // Import the CSS file
 
 function App() {
   return (
